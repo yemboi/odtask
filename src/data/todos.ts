1e872e65a039d1a49402dc1f246ca8cd2feae9fb
23046273 <y.akinyemi0920231@arts.ac.uk>
2025-11-25 14:59:07 +0000

    od-task

---
+++ src/data/todos.ts
@@ -0,0 +1,29 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+  id:1,
+    title: "Attend Introduction to expected fitness standards and Initial Military Fitness (IMF)",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Kit maintenance",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Complete marching and discipline classes",
+    completed: false, 
+  },
+  {
+  id: 4,
+  title:"Visit to a surface warship ",
+  completed: true,
+  },
+  {
+    id:5,
+    title: "Royal Navy Fitness Assessment",
+    completed: false
+  }
+];
